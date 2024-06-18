7ba3431a708b9e92c4c6e88259d539b6d3305e21
Uzma-Azam21 <uzmaazam241@gmail.com>
2024-06-18 06:46:21 -0700

    Update Project

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Cli-ATM
+Introduction:
+
+This command-line ATM application is developed using TypeScript/JavaScript and uses the inquirer library for user input. The application is designed to provide a user-friendly interface for performing various operations. 
+
+ 
+
+Usage:
+
+Obtain a copy of the repository by either cloning it via Git or downloading it directly. 
+
+Open your terminal and change to the directory where the project files are located. 
+
+Execute the command npm install to install all necessary dependencies specified in the project. 
+
+Start the ATM application by running node index.js in your terminal. 
+
+Respond to the prompts and choosing the operation you wish to perform. 
+
+Features:
+ 1- Withdraw Amount
+ a- Fast Cash
+ b- Enter your desired amount
+ 2- Check Balance
+ 3- Show remaining balance, after withdraw amount
+ 4- Detect invalid PIN Code
+
+
+Instruction: 
+
+To run this application, please ensure that Node.js is installed on your machine. 
+
+To use the features of this CLI-ATM application, you must enter the correct PIN code when prompted. The correct PIN code is “88661”.  
+
+
+ 
+
+ 
--- index.js
+++ index.js
@@ -4,78 +4,93 @@ import chalk from "chalk";
 // Initialize user balance and pin code.
 let myBalance = 15000; // (In Dollars)
 let myPin = 88661;
-// Print welcome message when user enter their pin code.
-console.log(chalk.blue.bold("\n \tWelcome back! How can we assist you today?\n"));
-let pinAnswer = await inquirer.prompt([
-    {
-        name: "pin",
-        type: "number",
-        message: chalk.green("Please enter your PIN code to access your account.")
-    }
-]);
-if (pinAnswer.pin === myPin) {
-    console.log(chalk.cyan("PIN verified, Please choose an option to continue."));
-    let operationAns = await inquirer.prompt([
+async function atmOperations() {
+    // Print welcome message when user enter their pin code.
+    console.log(chalk.blue.bold("\n \tWelcome back! How can we assist you today?\n"));
+    let pinAnswer = await inquirer.prompt([
         {
-            name: "operation",
-            type: "list",
-            message: chalk.grey("Please select options:"),
-            choices: ["Withdraw Amount", "Check Balance"]
+            name: "pin",
+            type: "number",
+            message: chalk.green("Please enter your PIN code to access your account.")
         }
     ]);
-    if (operationAns.operation === "Withdraw Amount") {
-        let withdrawAns = await inquirer.prompt([
-            {
-                name: "withdrawMethod",
-                type: "list",
-                message: chalk.yellow("Please select a withdrawal method:"),
-                choices: ["Fast Cash", "Enter Amount"]
-            }
-        ]);
-        if (withdrawAns.withdrawMethod === "Fast Cash") {
-            let fastCashAns = await inquirer.prompt([
+    if (pinAnswer.pin === myPin) {
+        console.log(chalk.cyan("\nPIN verified, Please choose an option to continue."));
+        let continueOperation = true;
+        while (continueOperation) {
+            let operationAns = await inquirer.prompt([
                 {
-                    name: "fastcash",
+                    name: "operation",
                     type: "list",
-                    message: chalk.green("Please enter the amount you wish to withdraw."),
-                    choices: [1000, 2000, 5000, 10000, 15000]
+                    message: chalk.grey("\nPlease select options:"),
+                    choices: ["Withdraw Amount", "Check Balance"]
                 }
             ]);
-            // If fastCashAns is greater than myBalance;
-            if (fastCashAns.fastcash > myBalance) {
-                console.log(chalk.red.bold("Unable to process. The amount exceeds your available balance."));
+            if (operationAns.operation === "Withdraw Amount") {
+                let withdrawAns = await inquirer.prompt([
+                    {
+                        name: "withdrawMethod",
+                        type: "list",
+                        message: chalk.yellow("\nPlease select a withdrawal method:"),
+                        choices: ["Fast Cash", "Enter Amount"]
+                    }
+                ]);
+                if (withdrawAns.withdrawMethod === "Fast Cash") {
+                    let fastCashAns = await inquirer.prompt([
+                        {
+                            name: "fastcash",
+                            type: "list",
+                            message: chalk.green("\nPlease enter the amount you wish to withdraw."),
+                            choices: [1000, 2000, 5000, 10000, 15000]
+                        }
+                    ]);
+                    // If fastCashAns is greater than myBalance;
+                    if (fastCashAns.fastcash > myBalance) {
+                        console.log(chalk.red.bold("\nUnable to process. The amount exceeds your available balance.\n"));
+                    }
+                    else {
+                        myBalance -= fastCashAns.fastcash; // TotalBalance = TotalBalance - fastCashAns
+                        console.log(chalk.cyan(`\nYou have withdrawn ${fastCashAns.fastcash}. Please collect your cash and receipt. Thank you for using our ATM.
+            `));
+                        console.log(`Your remaining balance is $${chalk.green.bold(myBalance)}.`);
+                    }
+                }
+                else if (withdrawAns.withdrawMethod === "Enter Amount") {
+                    let amountAns = await inquirer.prompt([
+                        {
+                            name: "amount",
+                            type: "number",
+                            message: chalk.green("\nPlease enter the amount you wish to withdraw.")
+                        }
+                    ]);
+                    // If amountAns is greater than myBalance;
+                    if (amountAns.amount > myBalance) {
+                        console.log(chalk.red("\nUnable to process. The amount exceeds your available balance."));
+                    }
+                    else {
+                        myBalance -= amountAns.amount; // TotalBalance = TotalBalance - amountAns
+                        console.log(chalk.cyan(`\nYou have withdrawn ${amountAns.amount}. Please collect your cash and receipt. Thank you for using our ATM.
+            `));
+                        console.log(`Your remaining balance is $${chalk.green.bold(myBalance)}.`);
+                    }
+                }
             }
-            else {
-                myBalance -= fastCashAns.fastcash; // TotalBalance = TotalBalance - fastCashAns
-                console.log(chalk.cyan(`You have withdrawn ${fastCashAns.fastcash}. Please collect your cash and receipt. Thank you for using our ATM.
-        `));
-                console.log(`Your remaining balance is $${chalk.green.bold(myBalance)}.`);
+            else if (operationAns.operation === "Check Balance") {
+                console.log(chalk.bold(`\nBalance check: Your available funds are $${chalk.blue.bold(myBalance)}.`));
             }
-        }
-        else if (withdrawAns.withdrawMethod === "Enter Amount") {
-            let amountAns = await inquirer.prompt([
+            let continueAns = await inquirer.prompt([
                 {
-                    name: "amount",
-                    type: "number",
-                    message: chalk.green("Please enter the amount you wish to withdraw.")
+                    name: "continue",
+                    type: "confirm",
+                    message: chalk.cyan.bold("\nDo you want to continue?")
                 }
             ]);
-            // If amountAns is greater than myBalance;
-            if (amountAns.amount > myBalance) {
-                console.log(chalk.red("Unable to process. The amount exceeds your available balance."));
-            }
-            else {
-                myBalance -= amountAns.amount; // TotalBalance = TotalBalance - amountAns
-                console.log(chalk.cyan(`You have withdrawn ${amountAns.amount}. Please collect your cash and receipt. Thank you for using our ATM.
-        `));
-                console.log(`Your remaining balance is $${chalk.green.bold(myBalance)}.`);
-            }
+            continueOperation = continueAns.continue;
         }
+        console.log(chalk.yellow.bold("\nThank you for using our ATM. Have a great day!"));
     }
-    else if (operationAns.operation === "Check Balance") {
-        console.log(`Balance check: Your available funds are $${chalk.blue.bold(myBalance)}.`);
+    else {
+        console.log(chalk.red.bold("\nIncorrect PIN. Please enter your PIN carefully.\n"));
     }
 }
-else {
-    console.log(chalk.red.bold("Incorrect PIN. Please enter your PIN carefully."));
-}
+atmOperations();
